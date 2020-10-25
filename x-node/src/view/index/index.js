4a5f2ee342c391dad59fb075634ff4c71d7f6150
bestkwanwa <bestkwanwa@gmail.com>
2020-10-25 11:07:51 +0800

    index nav done

--- x-node/src/view/index/index.js
+++ x-node/src/view/index/index.js
@@ -1,6 +1,8 @@
 import React, { Fragment } from 'react';
 import Nav from '../../component/Nav';
 import { indexNavs } from '../../router';
+import qs from 'qs';
+import { useLocation } from 'react-router-dom';
 export default function IndexPage() {
     return (
         <Fragment>
@@ -9,8 +11,16 @@ export default function IndexPage() {
             style={{
                 marginTop:'10px'
             }}
-            selected={()=>{
-                
+            selected={({search})=>{
+                search=qs.parse(search.substr(1))
+                let key=indexNavs.findIndex(item=>{
+                    let {tab}=qs.parse(item.to.substr(2))
+                    return tab==search.tab
+                })
+                if(search.tab===undefined){
+                    return '0'
+                }
+                return key+''
             }}
             ></Nav>
         </Fragment>
