2e316db6bb5254fe91b08185323c93edadd888f6
bestkwanwa <bestkwanwa@gmail.com>
2020-10-29 09:57:47 +0800

    project structure divided

--- x-node-plus/src/index.js
+++ x-node-plus/src/index.js
@@ -1,9 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import { BrowserRouter } from 'react-router-dom';
+import './static/css/index.css';
 import App from './App';
-
+import store from './store';
+import { Provider } from 'react-redux';
 ReactDOM.render(
-  <App />,
+  <Provider store={store}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </Provider>,
   document.getElementById('root')
 );
---
+++ x-node-plus/src/store/reducer/topics.js
@@ -0,0 +1,4 @@
+export default function topics(topics={},action){
+    return topics
+
+}
