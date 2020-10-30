bb27bf0f6351591004e5c8a208b3322f4f359016
bestkwanwa <bestkwanwa@gmail.com>
2020-10-30 15:15:51 +0800

    add the component of tag

--- x-node-plus/src/component/TopicList.js
+++ x-node-plus/src/component/TopicList.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { List, Col, Avatar } from 'antd';
 import { UserOutlined } from '@ant-design/icons';
 import { Link } from 'react-router-dom';
+import TopicTag from './TopicTag';
 export default function TopicList(props) {
     let { loading, data } = props
     return (
@@ -14,12 +15,15 @@ export default function TopicList(props) {
                 let { author, last_reply_at, good, top, tab, title, id } = data
                 let { loginname, avatar_url } = author
                 return <List.Item>
-                    <Col xs={24} md={20}>
+                    <Col xs={24} md={22}>
                         <Link to={`/user/${loginname}`}>
                             <Avatar title={loginname} src={avatar_url} icon={<UserOutlined />}></Avatar>
                         </Link>
+                        <TopicTag tab={top ? "top" : (good ? "good" : tab)}>
+                        </TopicTag>
+                        <Link to={`/topics/${id}`}>{title}</Link>
                     </Col>
-                    <Col xs={0} md={4}></Col>
+                    <Col xs={0} md={2}>日期时间</Col>
                 </List.Item>
             }}
         ></List>
